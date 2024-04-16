d0df637ce88a89b7691649e1e95810183c6ca9a0
Vsssel <asselonay@gmail.com>
2024-04-16 09:51:45 +0500

    commit before fixes

--- public/functions/socket.js
+++ public/functions/socket.js
@@ -1,7 +1,4 @@
 const socket = io()
-
-const clientsTotal = document.getElementById('client-total')
-
 const messageContainer = document.getElementById('message-container')
 const nameInput = document.getElementById('name-input')
 const messageForm = document.getElementById('message-form')
@@ -12,10 +9,6 @@ const messageTone = new Audio('/message-tone.mp3')
 messageForm.addEventListener('submit', (e) => {
   e.preventDefault()
   sendMessage()
-})
-
-socket.on('clients-total', (data) => {
-  clientsTotal.innerText = `Total Clients: ${data}`
 })
 
 function sendMessage() {
